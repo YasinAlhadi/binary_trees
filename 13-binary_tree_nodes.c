3d22839619dc54459a9b0a515fb4447a6ceb4485
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-12-13 23:42:20 +0000

    Nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
+
+/**
+ * binary_tree_nodes - counts the leaves in a binary tree.
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ *
+ * Return: 0 If tree is NULL.
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t left = 0;
+	size_t right = 0;
+
+	if (tree == NULL || binary_tree_is_leaf(tree))
+	{
+		return (0);
+	}
+	left = binary_tree_nodes(tree->left);
+	right = binary_tree_nodes(tree->left);
+	return (1 + left + right);
+}
