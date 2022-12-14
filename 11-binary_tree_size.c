ad7ba38bbe3b88b1c0ea1fc47358a203f311df5c
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-12-14 23:47:39 +0000

    Size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -14,6 +14,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	left = binary_tree_size(tree->left);
-	right = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
 	return (1 + left + right);
 }
